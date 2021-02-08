a1830b43b6cabfefd98bad0bbb7206aee61361e7
titammodswa <78710332+titammodswa@users.noreply.github.com>
2021-02-07 23:00:24 -0300

    Add files via upload

---
+++ titanWa.js
@@ -0,0 +1,18 @@
+{
+	"version": "1.0",
+	"download": "https://titammods.weebly.com/",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "changelog-pt.html"
+		}
+	]
+}
