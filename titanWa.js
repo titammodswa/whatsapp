9f236d125c24a0132a938d95db2e4ef56d3861ca
titammodswa <78710332+titammodswa@users.noreply.github.com>
2021-02-08 02:06:40 -0300

    Update titanWa.js

--- titanWa.js
+++ titanWa.js
@@ -3,6 +3,14 @@
 	"download": "https://titammods.weebly.com",
 	"update": [
 		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/titammodswa/whatsapp/main/changelog-pt.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/titammodswa/whatsapp/main/changelog-pt.html"
+		},
+		{
 			"languaje": "pt",
 			"changelog": "https://raw.githubusercontent.com/titammodswa/whatsapp/main/changelog-pt.html"
 		}
